b6bd1488b59121d28cec0ceac6a697701838ef91
I.Denisovs <i.denisovs@gmail.com>
2015-05-24 20:33:39 +0300

    Initial commit for DailyData.

---
+++ Source/API/src/main/java/lv/edreams/money/dal/DataProvider.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lv.edreams.money.dal;
+
+import lv.edreams.money.dal.dto.DailyData;
+
+/**
+ *
+ * @author Ga5Xz2
+ */
+public class DataProvider
+{
+    public DailyData getDailyData()
+    {
+        return new DailyData();
+    }
+}
--- Source/API/src/main/java/lv/edreams/money/dal/dto/DailyItem.java
+++ Source/API/src/main/java/lv/edreams/money/dal/dto/DailyItem.java
@@ -5,11 +5,95 @@
  */
 package lv.edreams.money.dal.dto;
 
+import java.util.Date;
+
 /**
  *
  * @author Ga5Xz2
  */
 public class DailyItem
 {
+    private Date date;
     
+    private double summary;
+    
+    private double expenses;
+    
+    private double rest;
+    
+    private double balance;
+    
+    private double dailyBalance;
+    
+    private double expectations;
+
+    public Date getDate()
+    {
+        return date;
+    }
+
+    public void setDate(Date date)
+    {
+        this.date = date;
+    }
+
+    public double getSummary()
+    {
+        return summary;
+    }
+
+    public void setSummary(double summary)
+    {
+        this.summary = summary;
+    }
+
+    public double getExpenses()
+    {
+        return expenses;
+    }
+
+    public void setExpenses(double expenses)
+    {
+        this.expenses = expenses;
+    }
+
+    public double getRest()
+    {
+        return rest;
+    }
+
+    public void setRest(double rest)
+    {
+        this.rest = rest;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public double getDailyBalance()
+    {
+        return dailyBalance;
+    }
+
+    public void setDailyBalance(double dailyBalance)
+    {
+        this.dailyBalance = dailyBalance;
+    }
+
+    public double getExpectations()
+    {
+        return expectations;
+    }
+
+    public void setExpectations(double expectations)
+    {
+        this.expectations = expectations;
+    }
 }
