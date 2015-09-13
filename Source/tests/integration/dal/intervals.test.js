7aa099f4d04b022881489a254b9fad1b8a08555e
I.Denisovs <i.denisovs@gmail.com>
2015-09-13 19:27:21 +0300

    DAL: Testing Intervals module integration.

---
+++ Source/tests/integration/dal/intervals.test.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Ga5Xz2 on 13.09.2015..
+ */
+var assert = require('chai').assert;
+var sinon = require('sinon');
+var intervals = require('../../../modules/dal/intervals');
+
+describe('Intervals module', function()
+{
+    //before(function() {
+    //    console.log('This should init the database!');
+    //});
+
+    it('getLatest function', function(done) {
+
+        intervals.getLatest(onSuccess, onError);
+
+        function onSuccess(data)
+        {
+            assert.isObject(data);
+            done();
+        }
+
+        function onError(error)
+        {
+            assert.fail(error);
+            done();
+        }
+    });
+});
