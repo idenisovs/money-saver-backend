73c0d42f7849a6de06459c15e169e849c424b75d
I.Denisovs <i.denisovs@gmail.com>
2015-12-21 23:12:55 +0200

    API: Totals Calculator done!

--- Source/modules/calculators/calculate-totals.js
+++ Source/modules/calculators/calculate-totals.js
@@ -1,9 +1,54 @@
 /**
  * Created by Ga5Xz2 on 19.12.2015..
  */
+var moment = require('moment');
+
 module.exports = calculateTotals;
 
-function calculateTotals()
+function calculateTotals(summary)
 {
+    var schedule = summary.schedule;
 
+    var totals =
+    {
+        days: schedule.length,
+        currentDay: getDaysDiff(summary.interval.start),
+        currentDayPercents: 0,
+        startingSum: summary.interval.sum,
+        expenses: 0
+    };
+
+    totals.currentDayPercents = totals.currentDay / totals.days;
+
+    var scheduleItem;
+
+    for (var d = 0; d < schedule.length; d++)
+    {
+        scheduleItem = schedule[d];
+
+        totals.expenses += scheduleItem.spent;
+    }
+
+    totals.expensesPercent = totals.expenses / totals.startingSum;
+
+    totals.residual = totals.startingSum - totals.expenses;
+
+    totals.residualPercents = totals.residual / totals.startingSum;
+
+    totals.expensesAvg = totals.expenses / totals.currentDay;
+
+    totals.incomesAvg = totals.residual / totals.days;
+
+    totals.expectedResidual =  totals.startingSum - (totals.days * totals.expensesAvg);
+
+    totals.expectedResidualPercents = totals.expectedResidual / totals.startingSum;
+
+    return totals;
+}
+
+function getDaysDiff(startingPoint)
+{
+    var days = moment().diff(startingPoint, 'days', true);
+
+    return Math.ceil(days);
 }
