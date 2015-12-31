be59395a960b226ee20bb261be7d0d6507042d73
I.Denisovs <i.denisovs@gmail.com>
2015-12-31 11:16:17 +0200

    API: Intervals - Save Intervals, added check, if Payment belongs to an Interval.

--- Source/modules/dal/payments/save-payment.js
+++ Source/modules/dal/payments/save-payment.js
@@ -1,5 +1,7 @@
+var util = require('util');
 var Promise = require('promise');
 var db = require('./../db');
+var getByTime = require('../intervals/get-by-time');
 
 var sql = "INSERT INTO payments (time, date, sum) VALUES ($time, $date, $sum)";
 
@@ -14,8 +16,27 @@ function savePayment(payment)
 	function resolver(_resolve, _reject)
 	{
 		resolve = _resolve;
-		
 		reject = _reject;
+
+		getByTime(payment.time, checkInterval);
+	}
+
+	function checkInterval(err, interval)
+	{
+		if (err)
+		{
+			reject(err);
+			return;
+		}
+
+		if (util.isUndefined(interval))
+		{
+			var message = '';
+			message += 'Given payment does not belong to any of intervals!\n';
+			message += 'Please, set the correct value to Time field!';
+			reject({ message: message, payment: payment });
+			return;
+		}
 
 		db.run(sql, params, done);
 	}
