37e415dab5d71bedaca272f656218719180087f4
Ilya Denisov <i.denisovs@gmail.com>
2016-01-05 17:51:20 +0200

    API: Added Logger configuration file and verbosity level switch.

--- Source/modules/support/logger.js
+++ Source/modules/support/logger.js
@@ -1,6 +1,26 @@
+var argv = require('../../argv.js');
 var log4js = require('log4js');
 
-var logger = log4js.getLogger('log');
+log4js.configure('logger.json', {});
+
+var consoleAppender = log4js.appenders.console();
+
+if (argv.v === 1)
+{
+	var infoAppender = log4js.appenders.logLevelFilter("INFO", "FATAL", consoleAppender);
+
+	log4js.addAppender(infoAppender);
+}
+
+if (argv.v > 1)
+{
+	console.log('extra verbose level');
+	
+	var traceAppender = log4js.appenders.logLevelFilter("TRACE", "FATAL", consoleAppender);
+
+	log4js.addAppender(traceAppender);
+}
+
 
 function getLogger(category)
 {
