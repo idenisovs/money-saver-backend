c15470f05f59e832a90b7e055f20fa25ebce9353
Ilya Denisov <i.denisovs@gmail.com>
2016-01-13 17:30:58 +0200

    run-integration-tests.sh file updated. Now it can run daemon.js service in testable mode and switch it off when tests is done.

--- Source/run-integration-tests.sh
+++ Source/run-integration-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#node daemon.js -b testable.db &
+
+#TESTABLE_NODE_PID=$!
 
 MOCHA_BIN=./node_modules/mocha/bin/mocha
 
 ./$MOCHA_BIN -c -r chai -R spec --recursive "tests/integration/**/*.test.js"
+
+#kill -9 $TESTABLE_NODE_PID
