9ce868c4f7d4fbc55d9f26bb35ad833a810ec9ae
Ilya Denisov <i.denisovs@gmail.com>
2016-01-13 17:27:30 +0200

    Integration tests: Delete Payments REST API tests.

--- Source/tests/integration/api/payments/delete-payment.js
+++ Source/tests/integration/api/payments/delete-payment.js
@@ -2,22 +2,105 @@
  * Created by Ga5Xz2 on 02.01.2016..
  */
 var util = require('util');
+var moment = require('moment');
 var request = require('request').defaults({ json: true });
 var assert = require('chai').assert;
 var host = require('../host.json').host;
+var helper = require('../helper/helper');
 
 function deletePaymentTests()
 {
-    it('Just test', justTest);
+	beforeEach(helper.createPayments);
+	
+    it('Till and From field specified', tillAndFromSpecified);
+	it('Delete payments by Id', deletePaymentsById);
+	it('Delete payments by interval Id', deletePaymentsByIntervalId);
+	
+	afterEach(helper.deletePayments);
 }
 
 module.exports = deletePaymentTests;
 
-function justTest(done)
+function tillAndFromSpecified(done)
 {
-    assert.equal(true, true);
+	var from = '2015-12-03';
+	var till = '2015-12-06';
+	var query = util.format('?from=%s&till=%s', from, till);
+    
+	var endpoint = util.format('%s?from=%s&till=%s', host.payments, from, till);
+	
+	request.del(endpoint, validate);
+	
+	function validate(err, res, body)
+	{
+		defaultValidation(err, res, body);
+		
+		assert.equal(body.removed, 6);
+		
+		done();
+	}
+}
 
-    done();
+function deletePaymentsById(done)
+{
+	var date = '2015-12-03';
+	
+	var endpoint = util.format('%s?from=%s&till=%s', host.payments, date, date);
+	
+	request.get(endpoint, processPaymentList);
+	
+	function processPaymentList(err, res, payments)
+	{
+		defaultValidation(err, res, payments);
+		
+		assert.equal(payments.length, 2);
+		
+		endpoint = util.format('%s?id=%s', host.payments, payments[0].id);
+		
+		request.del(endpoint, validate);
+	}
+	
+	function validate(err, res, body)
+	{
+		defaultValidation(err, res, body);
+		
+		assert.equal(body.removed, 1);
+		
+		done();
+	}
+}
+
+function deletePaymentsByIntervalId(done)
+{
+	var intervalId;
+	
+	var endpoint = host.intervals + '/latest';
+	
+	request.get(endpoint, onIntervalIdReceived);
+	
+	function onIntervalIdReceived(err, res, interval)
+	{
+		defaultValidation(err, res, interval);
+		
+		assert.property(interval, 'id');
+		
+		intervalId = interval.id;
+		
+		endpoint = util.format('%s?intervalId=%d', host.payments, intervalId);
+		
+		request.del(endpoint, validate);
+	}
+	
+	function validate(err, res, body)
+	{
+		defaultValidation(err, res, body)
+		
+		assert.property(body, 'removed');
+		
+		assert.equal(body.removed, 15);
+		
+		done();
+	}
 }
 
 function defaultValidation(err, res, body)
