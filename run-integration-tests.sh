2bd77ce730b8bd32441080cbcb44ae1d2f133d96
Ilya Denisov <i.denisovs@gmail.com>
2016-02-01 15:30:10 +0200

    run-integration-tests.sh - now it run and stop testable daemon.js

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -2,14 +2,14 @@
 
 cd Source
 
-#node daemon.js -b testable.db &
+node daemon.js -b testable.db &
 
-#TESTABLE_NODE_PID=$!
+TESTABLE_NODE_PID=$!
 
 MOCHA_BIN=../node_modules/mocha/bin/mocha
 
 ./$MOCHA_BIN -c -r chai -R spec --recursive "tests/integration/**/*.test.js"
 
-#kill -9 $TESTABLE_NODE_PID
+kill -9 $TESTABLE_NODE_PID
 
 cd ..
