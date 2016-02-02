aaad89d0490835af0388099938a0355a5d91772d
Ilya Denisov <i.denisovs@gmail.com>
2016-02-02 14:51:23 +0200

    API: Authentication shall be implemented by using of Passport.js module.

--- Source/api/api.js
+++ Source/api/api.js
@@ -4,8 +4,6 @@ router.use('/payments', require('./payments/payments'));
 
 router.use('/intervals', require('./intervals/intervals'));
 
-router.use('/authentication', require('./authentication/authentication'));
-
 module.exports = router;
 
 require('log4js').getLogger('api').info('API is up!');
