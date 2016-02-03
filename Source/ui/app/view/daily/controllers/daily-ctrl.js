6236a5b0ed93d61feb5664349a1c65c9d24bb181
Ilya Denisov <i.denisovs@gmail.com>
2016-02-03 17:44:52 +0200

    UI: Refactoring on daily controller.

--- Source/ui/app/view/daily/controllers/daily-ctrl.js
+++ Source/ui/app/view/daily/controllers/daily-ctrl.js
@@ -1,54 +1,67 @@
 var app = angular.module('MoneySaverApp');
 
-app.controller('DailyCtrl', ['$scope', '$modal', '$log', 
-	function($scope, $modal, $log){
+app.controller('DailyCtrl', dailyController);
 
-        $scope.datePicker = {
-            opened: false,
-            format: 'dd.MM.yyyy.',
-            options: {
-                formatYear: 'yyyy',
-                startingDay: 1
-            },
-            minDate: new Date(2015, 0, 1),
-            open: function($event) {
-                $event.preventDefault();
-                $event.stopPropagation();
+dailyController.$inject = [ '$scope', '$modal', '$log' ];
 
-                $scope.datePicker.opened = true;
-            }
-        };
+function dailyController($scope, $modal, $log)
+{
+	$scope.datePicker = {
+		opened: false,
+		format: 'dd.MM.yyyy.',
+		options: {
+			formatYear: 'yyyy',
+			startingDay: 1
+		},
+		minDate: new Date(2015, 0, 1),
+		open: datePickerOpen
+	};
 
-        $scope.today = function() {
-            $scope.dt = new Date();
-        };
+	$scope.viewNewIntervalModal = viewNewIntervalModal;
+	$scope.today = today;
 
-        $scope.today();
-
-		$scope.viewNewIntervalModal = function() {
-			$modal.open({
-				animation: true,
-				size: 'md',
-				templateUrl: 'app/view/daily/templates/interval.modal.html',
-				controller: 'IntervalModalCtrl'
-			});
-		};
+	today();
+	
+	function today()
+	{
+		$scope.dt = new Date();
 	}
-]);
-
-app.controller('IntervalModalCtrl', ['$scope', '$modalInstance', '$log', 
-	function($scope, $modalInstance, $log){
-		//$log.info('Kkk');
-		$scope.selected = { item: 'Hello, world!' };
-		
-		$scope.ok = function() {
-			//$log.info('ok');
-			$modalInstance.close();
+	
+	function datePickerOpen($event)
+	{
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.datePicker.opened = true;
+	}
+	
+	function viewNewIntervalModal()
+	{
+		var options = {
+			animation: true,
+			size: 'md',
+			templateUrl: 'app/view/daily/templates/interval.modal.html',
+			controller: 'IntervalModalCtrl'
 		};
 		
-		$scope.cancel = function() {
-			//$log.info('cancel');
-			$modalInstance.dismiss('cancel');
-		};
+		$modal.open(options);
 	}
-]);
+}
+
+app.controller('IntervalModalCtrl', intervalModalController);
+
+intervalModalController.$inject = [ '$scope', '$modalInstance', '$log' ];
+
+function intervalModalController($scope, $modalInstance, $log)
+{
+	$scope.selected = { item: 'Hello, world!' };
+		
+	$scope.ok = function() {
+		//$log.info('ok');
+		$modalInstance.close();
+	};
+	
+	$scope.cancel = function() {
+		//$log.info('cancel');
+		$modalInstance.dismiss('cancel');
+	};
+}
