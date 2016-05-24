4d45d33ed050cb0287eb874edeb701c0863ebf59
Ilya Denisov <i.denisovs@gmail.com>
2016-05-24 22:29:46 +0300

    passport.js - removed unnecessary log calls.

--- Source/support/passport.js
+++ Source/support/passport.js
@@ -19,29 +19,20 @@ log.info('Configured!');
 
 function serialize(user, done)
 {
-    log.debug('Serializing user %s.', user.login);
-    log.trace(user);
-
     done(null, user.id);
 }
 
 function deserialize(id, done)
 {
-    log.debug('Deserialize of user Nr. %d...', id);
-
     bl.users.getById(id, success, error);
 
     function success(user)
     {
-        log.debug('Deserialize of user Nr. %d is done!', id);
-        log.trace(user);
-
         done(null, user);
     }
 
     function error(err)
     {
-        log.error('Deserialize of user Nr. %d is failed!', id);
         done(err);
     }
 }
