606f701784e8aa83758ab49be67d8bea2ee6b0c6
Ilya Denisov <i.denisovs@gmail.com>
2016-05-24 22:02:05 +0300

    API: Validating password hash with BCrypt.

--- Source/bl/auth/auth-local.js
+++ Source/bl/auth/auth-local.js
@@ -2,32 +2,61 @@
  * Created by I.Denisovs on 16.17.5.
  */
 
+var bcrypt = require('bcrypt-nodejs');
 var log = require('log4js').getLogger('auth');
 var dal = require('../../dal/dal');
 
+module.exports = auth;
+
 function auth(username, password, done)
 {
+    var user = null;
+
     log.debug('Authorization of user %s...', username);
 
-    dal.users.getByName(username, callback);
+    dal.users.getByName(username, validateUsername);
 
-    function callback(error, user)
+    function validateUsername(error, userRecord)
     {
         if (error)
         {
-            log.error(error);
-            return done(error);
+            return fail(error);
         }
 
-        if (user.login === username && user.password === password)
+        user = userRecord;
+
+        if (user === null)
         {
-            log.debug('User %s successfully authorized!', username);
-            return done(null, user);
+            return reject();
         }
 
+        bcrypt.compare(password, user.password, validatePassword);
+    }
+
+    function validatePassword(error, passValid)
+    {
+        if (error)
+        {
+            return fail(error);
+        }
+
+        if (!passValid)
+        {
+            return reject();
+        }
+
+        done(null, user)
+    }
+
+    function reject()
+    {
         log.warn('Rejecting user %s with password %s!', username, password);
         done(null, false, { message: 'Incorrect login or password!' });
     }
-}
 
-module.exports = auth;
+    function fail(error)
+    {
+        log.error(error);
+        done(error);
+    }
+}
