783fff83c841fed748ed74b1b7d0ecb384f0b677
I. Denisovs <i.denisovs@gmail.com>
2016-05-30 23:38:29 +0300

    BL: Added some log information to get-payments module.

--- Source/bl/payments/get-payments.js
+++ Source/bl/payments/get-payments.js
@@ -4,6 +4,7 @@
 
 var util = require('util');
 var moment = require('moment');
+var log = require('log4js').getLogger('payments');
 var dal = require('../../dal/dal');
 
 module.exports = getPayments;
@@ -12,32 +13,35 @@ function getPayments(request, user, success, error)
 {
     if ('id' in request)
     {
+        log.debug('Taking payments by Id!');
         dal.payments.getById(request.id, user.id, done);
         return;
     }
 
     if ('date' in request)
     {
+        log.debug('Taking payments by date!');
         dal.payments.getByDate(request.date, user.id, done);
         return;
     }
 
     if (('from' in request) && ('till' in request))
     {
+        log.debug('Taking payments by date range!');
         var from = moment(request.from).startOf('day').valueOf();
         var till = moment(request.till).endOf('day').valueOf();
         dal.payments.getByDateRange(from, user.id, till, done);
         return;
     }
 
+    log.debug('Taking payments by latest interval!');
     dal.intervals.getLatest(user.id, getLatestDone);
 
     function getLatestDone(err, latestInterval)
     {
         if (err)
         {
-            error(err);
-            return;
+            return error(err);
         }
 
         dal.payments.getByIntervalId(latestInterval.id, user.id, done);
@@ -47,8 +51,7 @@ function getPayments(request, user, success, error)
     {
         if (err)
         {
-            error(err);
-            return;
+            return error(err);
         }
 
         success(payments);
