98f8ca5fd78948d915afc985a92263a0c42ae44c
Ilya Denisov <i.denisovs@gmail.com>
2016-06-18 19:29:14 +0300

    daemon.js - structure changes.

--- Source/daemon.js
+++ Source/daemon.js
@@ -12,26 +12,44 @@ var app = express();
 
 log.info('Launching daemon...');
 
-var static = express.static(config.content.public, { index: config.index });
+enable3dPartyMiddleware();
 
-app.use(static);
+enableStaticContent();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(session({ secret: 'Fb75JGeUyTlEuCMp', resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+enableRestAPI();
 
-app.use('/api', require('./api/api.js'));
-
-if (util.isNumber(argv.port))
-{
-    config.port = argv.port;
-}
+config.port = util.isNumber(argv.port) ? argv.port : config.port;
 
 var server = app.listen(config.port, onListen);
 
 function onListen()
 {
 	log.info('Daemon started and listening on %s port', server.address().port);
+}
+
+function enable3dPartyMiddleware()
+{
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(session({ secret: 'Fb75JGeUyTlEuCMp', resave: false, saveUninitialized: true }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    log.debug('3d party middleware enabled!');
+}
+
+function enableStaticContent()
+{
+    var staticAccessControl = require('./middleware/static-access-control');
+    var static = express.static(config.content.public, { index: config.index });
+    app.use(staticAccessControl, static);
+
+    log.debug('Static content enabled!');
+}
+
+function enableRestAPI()
+{
+    app.use('/api', require('./api/api.js'));
+
+    log.debug('REST API enabled!');
 }
