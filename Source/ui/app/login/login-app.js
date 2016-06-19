3a33aee3fe1b0973c9f741718e6c828f2de08fa0
Ilya Denisov <i.denisovs@gmail.com>
2016-06-19 13:50:49 +0300

    UI: Show warning message in login page if user try to get application without login.

--- Source/ui/app/login/login-app.js
+++ Source/ui/app/login/login-app.js
@@ -1,27 +1,30 @@
 /**
  * Created by I.Denisovs on 16.13.6.
  */
-var app = angular.module('MoneySaverAppLogin', ['ui.bootstrap', 'ngCookies', 'ngResource']);
+var deps = ['ui.bootstrap', 'ngCookies', 'ngResource', 'ngRoute'];
+
+var app = angular.module('MoneySaverAppLogin', deps);
 
 app.controller('login-ctrl', loginCtrl);
 
-loginCtrl.$inject = ['$scope', '$timeout', 'login-service', '$log', '$window'];
+loginCtrl.$inject = ['$scope', '$timeout', 'login-service', '$log', '$window', '$routeParams'];
 
-function loginCtrl($scope, $timeout, loginService, $log, $window)
+function loginCtrl($scope, $timeout, loginService, $log, $window, $routeParams)
 {
     $scope.login = '';
     $scope.password = '';
     $scope.showWarning = false;
     $scope.showLoginFailed = false;
+    $scope.showLoginRequierd = false;
     $scope.message = 'Hello, world!';
     $scope.performLogin = performLogin;
     $scope.emptyCredentials = isCredentialsEmpty;
 
-    $timeout(warning, 2000);
+    var queryParams = getQueryString();
 
-    function warning()
+    if ('not-authorized' in queryParams)
     {
-        $scope.showWarning = true;
+        $scope.showLoginRequierd = true;
     }
 
     function performLogin()
@@ -47,5 +50,30 @@ function loginCtrl($scope, $timeout, loginService, $log, $window)
     function isCredentialsEmpty()
     {
         return $scope.login.length === 0 || $scope.password.length === 0;
+    }
+
+    function getQueryString()
+    {
+        var urlParams = {};
+
+        var location = $window.location.href;
+
+        if (location.indexOf('?') === -1)
+        {
+            return urlParams;
+        }
+
+        var match,
+            pl     = /\+/g,  // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+            query  = location.split('?')[1];
+
+        while (match = search.exec(query))
+        {
+            urlParams[decode(match[1])] = decode(match[2]);
+        }
+
+        return urlParams;
     }
 }
