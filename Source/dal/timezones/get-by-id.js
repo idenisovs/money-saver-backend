1d1410607cd67a286f761b80a9d9a738e9d811e6
Ilya Denisov <i.denisovs@gmail.com>
2016-12-07 20:00:15 +0200

    API: timezones.json - Number values is stored as numbers.

--- Source/dal/timezones/get-by-id.js
+++ Source/dal/timezones/get-by-id.js
@@ -2,7 +2,7 @@ var timezones = require('./timezones.json');
 
 function getById(id, done) {
 	for (var i = 0; i < timezones.length; i++) {
-		if (timezones[i].timeZoneId === id) {
+		if (timezones[i].timeZoneId == id) {
 			return done(null, timezones[i]);
 		}
 	}
