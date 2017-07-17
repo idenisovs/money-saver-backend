253c516318f4de8367c50b469f9fdb8b3efc55ca
Ilja Denisovs <i.denisovs@gmail.com>
2017-07-17 19:01:23 +0300

    Refactoring: Save Payment module is using the Deferred instead of raw Promise.

--- Source/dal/payments/save-payment.js
+++ Source/dal/payments/save-payment.js
@@ -1,60 +1,48 @@
+const deferred = require('deferred');
 const util = require('util');
-const Promise = require('promise');
 const log = require('../../support/logger')();
 const db = require('./../db');
 const getIntervalByTime = require('../intervals/get-by-time');
 
-var sql = "INSERT INTO payments (time, date, sum, userId) VALUES ($time, $date, $sum, $userId)";
+const sql = "INSERT INTO payments (time, date, sum, userId) VALUES ($time, $date, $sum, $userId)";
 
 function savePayment(payment)
 {
-	var resolve, reject; 
+	const q = deferred();
 
 	log.debug('Saving payment with sum: %s and user id %s', payment.sum, payment.user.id);
 	log.trace(JSON.stringify(payment));
 
-	var params = { $time: payment.time, $date: payment.date, $sum: payment.sum, $userId: payment.user.id };
+	const params = { $time: payment.time, $date: payment.date, $sum: payment.sum, $userId: payment.user.id };
+    const interval = { time: payment.time, user: payment.user };
 
-	return new Promise(resolver);
-	
-	function resolver(_resolve, _reject)
-	{
-		resolve = _resolve;
-		reject = _reject;
+    getIntervalByTime(interval, checkInterval);
 
-		var interval = { time: payment.time, user: payment.user };
-
-		getIntervalByTime(interval, checkInterval);
-	}
+	return q.promise;
 
 	function checkInterval(err, interval)
 	{
-		if (err)
-		{
+		if (err) {
 			return reject(err);
 		}
 
-		if (util.isUndefined(interval))
-		{
-			var message = '';
-			message += 'Given payment does not belong to any of intervals!\n';
-			message += 'Please, set the correct value to Time field!';
-			delete payment.user;
-			reject({ message: message, payment: payment });
-			return;
+		if (interval) {
+            return db.run(sql, params, done);
 		}
 
-		db.run(sql, params, done);
+        let message = '';
+        message += 'Given payment does not belong to any of intervals!\n';
+        message += 'Please, set the correct value to Time field!';
+        delete payment.user;
+        q.reject({ message: message, payment: payment });
 	}
 	
-	function done(err)
-	{
-		if (err)
-		{
-			return reject(err);
+	function done(err) {
+		if (err) {
+			return q.reject(err);
 		}
 		
-		resolve();
+		q.resolve();
 	}
 }
 
