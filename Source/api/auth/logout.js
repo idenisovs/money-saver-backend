787e5ff4d13506d3b0e798a754d13b61811572ed
Ilya Denisov <i.denisovs@gmail.com>
2019-07-22 13:03:47 +0300

    Modeling NavBar component.

--- Source/api/auth/logout.js
+++ Source/api/auth/logout.js
@@ -2,15 +2,14 @@
  * Created by I.Denisovs on 16.30.5.
  */
 
-var log = require('log4js').getLogger('auth');
+const log = require('log4js').getLogger('auth');
 
-function logout(req, res)
-{
+function logout(req, res) {
     log.debug('User %s logging out!', req.user.login);
 
     req.logout();
 
-    res.redirect('/');
+    res.redirect('/login');
 }
 
 module.exports = logout;
