705741f93d33377f974934b44deb0e9fc9c107c3
I.Denisovs <ide@adevo.io>
2020-08-30 15:11:12 +0300

    Rollback of default behaviour for getUserAuth.

--- Source/api/auth/get-user-auth.js
+++ Source/api/auth/get-user-auth.js
@@ -4,9 +4,7 @@ module.exports = function getUserAuthentication(req, res) {
     log.debug('Requested user auth status!');
     log.trace(req.user);
 
-    const user = req.user || {
-        name: 'abc def'
-    };
+    const user = req.user || null;
 
     if (user) {
         delete user.password;
