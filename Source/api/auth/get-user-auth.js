61e029c98122db9679d8c2674cb16d132c82366a
I.Denisovs <ide@adevo.io>
2020-09-11 13:03:12 +0300

    getUserAuth API returning actual Auth information.

--- Source/api/auth/get-user-auth.js
+++ Source/api/auth/get-user-auth.js
@@ -10,8 +10,5 @@ module.exports = function getUserAuthentication(req, res) {
         delete user.password;
     }
 
-    // res.json(user);
-    res.status(500).json({
-        message: 'Something went wrong on the server!'
-    });
+    res.json(user);
 };
