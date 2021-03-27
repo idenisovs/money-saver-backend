3cbe54cc98c5e17bfacbe2fc853fcdcaa7fc85a7
Ilya Denisov <i.denisovs@gmail.com>
2021-03-27 13:30:55 +0200

    Spawning PM2 process during setup.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
-      'post-setup': 'npm install'
+      'post-setup': 'npm run setup && pm2 start ./ecosystem.config.js && pm2 save'
     }
   }
 };
