ffd261a6379407b7fd2043334fe78ef794868e3d
Ilya Denisov <i.denisovs@gmail.com>
2021-06-17 16:22:18 +0300

    Fixed Argv TypeScript error.

---
+++ source/support/arguments.ts
@@ -0,0 +1,8 @@
+export interface Arguments {
+	port: number;
+	database: string;
+	memcached: boolean;
+	verbose: boolean;
+	debug: boolean;
+	trace: boolean;
+}
---
+++ source/support/argv.ts
@@ -0,0 +1,13 @@
+import yargs from 'yargs';
+import { Arguments } from './arguments';
+
+export default yargs
+	.usage('Launch Money Saver')
+	.options({
+	port: { type: 'number', default: 9001, alias: 'p', describe: 'Select port to listen' },
+	database: { type: 'string', alias: 'b', describe: 'Select database to work with' },
+	memcached: { type: 'boolean', default: false, alias: 'm', describe: 'Enabled memcached support' },
+	verbose: { type: 'boolean', default: false, alias: 'v', describe: 'Run in Verbose mode' },
+	debug: { type: 'boolean', default: false, alias: 'd', describe: 'Set log level to DEBUG' },
+	trace: { type: 'boolean', default: false, alias: 't', describe: 'Set log level to TRACE, extra verbosity level.' }
+}).argv as Arguments;
