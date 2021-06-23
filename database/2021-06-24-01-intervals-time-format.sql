f5d7579303a73fea950fff96e6a7f52397ed3b98
Ilya Denisov <i.denisovs@gmail.com>
2021-06-23 21:40:24 +0300

    Fix for db update script intervals-time-format.sql

--- database/2021-06-24-01-intervals-time-format.sql
+++ database/2021-06-24-01-intervals-time-format.sql
@@ -1,3 +1,3 @@
 UPDATE intervals
-SET start = strftime('%Y-%m-%dT%H:%M:%S.%fZ', start / 1000, 'unixepoch'),
-    [end] = strftime('%Y-%m-%dT%H:%M:%S.%fZ', [end] / 1000, 'unixepoch');
+SET start = strftime('%Y-%m-%dT%H:%M:%fZ', start / 1000, 'unixepoch'),
+    [end] = strftime('%Y-%m-%dT%H:%M:%fZ', [end] / 1000, 'unixepoch');
