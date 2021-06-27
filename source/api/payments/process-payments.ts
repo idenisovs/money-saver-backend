e9d549a441e84fcfbd721a6d927070606597153e
Ilya Denisov <i.denisovs@gmail.com>
2021-06-27 18:16:55 +0300

    Different minor fixes for Dashboard and Expenses list.

--- source/api/payments/process-payments.ts
+++ source/api/payments/process-payments.ts
@@ -5,13 +5,13 @@ import { Request, Response } from 'express';
 import { User } from '../../shared';
 import processPaymentDtos from './process-payment-dtos';
 
-const log = log4js.getLogger('save-payments');
+const log = log4js.getLogger('process-payments');
 
 export default async function processPayments(req: Request, res: Response) {
 	const user = req.user as User;
 	const payments = processPaymentDtos(req.body);
 
-	log.debug('User <%d> made request to process <%d> payments!', user.id, payments.length);
+	log.info('User <%d> made request to process <%d> payments!', user.id, payments.length);
 
 	try {
 		await bl.payments.process(payments, user);
