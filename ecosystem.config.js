cbdec0565a534c9c1c789088e559898da11b5c6e
Ilya Denisov <i.denisovs@gmail.com>
2021-06-27 16:32:04 +0300

    PM2 config got submodule update instructions for setup stage.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       repo : 'git@github.com:idenisovs/money-saver-backend.git',
       path : '/opt/pm2/money-saver',
       'post-deploy' : 'git submodule update && npm run update && pm2 startOrRestart ./ecosystem.config.js',
-      'post-setup': 'npm run setup && pm2 save'
+      'post-setup': 'git submodule update --init && npm run setup && pm2 save'
     }
   }
 };
