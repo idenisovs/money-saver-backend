d5b41d057a5872fbcaccc44ed56674c74fd76fe4
Ilya Denisov <i.denisovs@gmail.com>
2021-06-27 18:59:27 +0300

    Fixing issue for already closed database connection.

--- source/dal/connectors/sqlite-exit-handler.ts
+++ source/dal/connectors/sqlite-exit-handler.ts
@@ -3,14 +3,22 @@ import sqlite3 from 'sqlite3';
 
 const log = log4js.getLogger('db');
 
-export default function exitHandler(connector: sqlite3.Database) {
+let isClosed = false;
+
+export default function exitHandler(db: sqlite3.Database) {
     return function handler() {
+        if (isClosed) {
+            return;
+        }
+
+        isClosed = true;
+
         process.removeListener('exit', handler);
         process.removeListener('SIGINT', handler);
 
         log.info('Closing database...');
 
-        connector.close(closeDone);
+        db.close(closeDone);
     }
 
     function closeDone(err: Error|null) {
