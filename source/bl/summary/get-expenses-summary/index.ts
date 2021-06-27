8d81196396c3a32cd4d0f72857790d29c494cc37
Ilya Denisov <i.denisovs@gmail.com>
2021-06-27 17:10:41 +0300

    If no intervals available, return NULL for summary.

--- source/bl/summary/get-expenses-summary/index.ts
+++ source/bl/summary/get-expenses-summary/index.ts
@@ -5,12 +5,21 @@ import makeSchedule from './make-schedule';
 import calculateSchedule from './calculate-schedule';
 import calculateTotals from './calculate-totals';
 
-const log = log4js.getLogger('payments-summary');
+const log = log4js.getLogger('expenses-summary');
 
-export async function getExpensesSummary(intervalId: number, user: User): Promise<Summary> {
-	log.debug('Calculating summary for interval <%d> of user <%d>!', intervalId, user.id);
+export async function getExpensesSummary(intervalId: number, user: User): Promise<Summary|null> {
+	if (intervalId) {
+		log.debug('Calculating summary for interval <%d> of user <%d>!', intervalId, user.id);
+	} else {
+		log.debug('Calculating summary for latest interval of user <%d>!', user.id);
+	}
 
 	const interval = await getInterval(intervalId, user);
+
+	if (!interval) {
+		log.warn('User <%d> have no intervals yet, stopping!', user.id);
+		return null;
+	}
 
 	const summary = Summary.fromInterval(interval);
 
