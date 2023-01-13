215bbd3627ea6f522ae1ee383af8584ecd0b4435
Ilja Denisovs <i.denisovs@gmail.com>
2023-01-13 15:25:22 +0200

    Logout function fix. Added some logging.

--- source/api/auth/logout.ts
+++ source/api/auth/logout.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
-import { User } from '../../shared';
+import HttpStatusCodes from 'http-status';
 import log4js from 'log4js';
+
+import { User } from '../../shared';
 
 const log = log4js.getLogger('auth');
 
@@ -9,7 +11,15 @@ export default function logout(req: Request, res: Response) {
 
     log.debug('User %s is logging out!', user.login);
 
-    req.logout();
+    req.logout(done);
 
-    res.send();
+    function done(err: any) {
+        if (err) {
+            log.error('Something bad happened during logout!');
+            log.error(err);
+            res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+        }
+
+        res.send();
+    }
 }
