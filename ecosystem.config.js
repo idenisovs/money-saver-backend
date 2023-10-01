ac92c45ab2e21884f8bbd176c3ef7b7b8c31b385
Ilja Denisovs <i.denisovs@gmail.com>
2023-10-01 17:35:42 +0300

    PM2 deployment got Clean step.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:idenisovs/money-saver-backend.git',
       path : '/opt/pm2/money-saver',
-      'post-deploy' : 'git submodule update && npm run update && pm2 startOrRestart ./ecosystem.config.js',
+      'post-deploy' : 'git submodule update && npm run clean && npm run update && pm2 startOrRestart ./ecosystem.config.js',
       'post-setup': 'git submodule update --init && npm run setup'
     }
   }
