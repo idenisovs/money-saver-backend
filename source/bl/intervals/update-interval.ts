f18703da3e4e78525753d6b4d5edf413de91341e
Ilja Denisovs <i.denisovs@gmail.com>
2023-10-22 14:45:02 +0300

    Update Interval API endpoint is not setting the interval to the end of the day.

--- source/bl/intervals/update-interval.ts
+++ source/bl/intervals/update-interval.ts
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import log4js from 'log4js';
 import { Interval, User } from '../../shared';
 import dal from '../../dal';
@@ -7,8 +6,6 @@ const log = log4js.getLogger('update-interval');
 
 export async function updateInterval(interval: Interval, user: User): Promise<void> {
 	log.trace(interval);
-
-	interval.end = moment(interval.end).endOf('day').toDate();
 
 	await dal.intervals.update(interval, user);
 }
