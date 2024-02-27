3ee8170e74dff46f12d2f946fa930ca5ab3b98d8
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-27 15:51:36 +0200

    Running tests in UTC timezone.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  setupFiles: ['./jest.setup.js'],
+  globalSetup: './jest.setup.js',
+  setupFiles: [],
   preset: 'ts-jest',
+  testMatch: ['**/*.test.ts'],
   testEnvironment: 'node',
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,5 @@
-require('dotenv').config({
-    path: '.env.test'
-})
+module.exports = () => {
+    require('dotenv').config({
+        path: '.env.test'
+    })
+};
