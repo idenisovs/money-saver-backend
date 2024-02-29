74e613dd8925e3bdac524ffb75210f59f009fa2b
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-29 13:39:00 +0200

    Pulling interval start and end dates to their boundaries.

--- source/bl/intervals/update-interval.ts
+++ source/bl/intervals/update-interval.ts
@@ -1,11 +1,15 @@
 import log4js from 'log4js';
 import { Interval, User } from '../../shared';
 import dal from '../../dal';
+import { endOfDay, startOfDay } from '../../shared/utils';
 
 const log = log4js.getLogger('update-interval');
 
 export async function updateInterval(interval: Interval, user: User): Promise<void> {
 	log.trace(interval);
+
+	interval.start = startOfDay(interval.start, user.timezone);
+	interval.end = endOfDay(interval.end, user.timezone);
 
 	await dal.intervals.update(interval, user);
 }
