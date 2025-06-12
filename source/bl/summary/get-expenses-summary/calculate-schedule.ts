6c7c962d4c25c0578b3bab1638d22665c2d2a234
Ilja Denisovs <i.denisovs@gmail.com>
2025-06-12 12:21:04 +0300

    Scheduler calculator now got capabilities to make a forecast for residuals.

--- source/bl/summary/get-expenses-summary/calculate-schedule.ts
+++ source/bl/summary/get-expenses-summary/calculate-schedule.ts
@@ -1,18 +1,20 @@
-import { Summary } from '../../../shared';
+import { SimpleLinearRegression } from 'ml-regression-simple-linear';
+
+import { DailyExpensesOverview, Summary } from '../../../shared';
 
 export default function calculateSchedule(summary: Summary) {
 	const { dailyExpenses } = summary;
 	const dailySum = summary.interval.sum / dailyExpenses.length;
 
-	let residual = summary.interval.sum;
+	let residuals = summary.interval.sum;
 	let balance = 0;
 
 	for (let idx = 0; idx < dailyExpenses.length; idx += 1) {
 		const day = dailyExpenses[idx];
 
-		residual -= day.expenses;
+		residuals -= day.expenses;
 
-		day.residual.real = residual;
+		day.residual.real = residuals;
 		day.residual.planned = summary.interval.sum - dailySum * (idx + 1);
 
 		day.balance.daily = dailySum - day.expenses;
@@ -20,5 +22,30 @@ export default function calculateSchedule(summary: Summary) {
 		balance += day.balance.daily;
 
 		day.balance.total = balance;
+	}
+
+	const forecastingInput = dailyExpenses.map((dailyExpenses: DailyExpensesOverview, idx: number) => ({
+		x: idx,
+		y: dailyExpenses.residual.real,
+		expenses: dailyExpenses.expenses
+	}));
+
+	for (let idx = forecastingInput.length - 1; idx >= 0; idx--) {
+		const forecastingItem = forecastingInput[idx];
+		if (forecastingItem.expenses) {
+			break;
+		} else {
+			forecastingInput.splice(idx, 1);
+		}
+	}
+
+	const x = forecastingInput.map((row) => row.x);
+	const y = forecastingInput.map((row) => row.y);
+
+	const slr = new SimpleLinearRegression(x, y);
+
+	for (let idx = 0; idx < dailyExpenses.length; idx += 1) {
+		const day = dailyExpenses[idx];
+		day.residual.forecast = slr.predict(idx);
 	}
 }
