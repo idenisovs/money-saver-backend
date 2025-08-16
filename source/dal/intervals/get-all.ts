cbd7981e80fa47907fbcbe7639f05f3888a129fd
Ilja Denisovs <i.denisovs@gmail.com>
2025-08-16 16:09:25 +0300

    Apply Promise.withResolvers in getAll intervals DAL.

--- source/dal/intervals/get-all.ts
+++ source/dal/intervals/get-all.ts
@@ -7,11 +7,13 @@ import IntervalRecord from './interval-record';
 const sql = 'SELECT id, start, end, sum, latest FROM intervals WHERE userId = $userId ORDER BY start DESC';
 
 export function getAll(user: User): Promise<Interval[]> {
-	return new Promise((resolve, reject) => {
-		const params = {
-			$userId: user.id,
-		};
+	const { promise, resolve, reject } = Promise.withResolvers<Interval[]>();
 
-		db.all(sql, params, done<IntervalRecord, Interval>(resolve, reject, intervalMapper));
-	});
+	const params = {
+		$userId: user.id,
+	};
+
+	db.all(sql, params, done<IntervalRecord, Interval>(resolve, reject, intervalMapper));
+
+	return promise;
 }
