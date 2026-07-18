f3ac80101ae4a11bb17d053e3c114b6c872ea69a
Ilja Denisovs <i.denisovs@gmail.com>
2026-07-18 20:08:55 +0300

    Remove yargs usage from make-password script.

--- scripts/make-password.js
+++ scripts/make-password.js
@@ -1,30 +1,41 @@
 #!/usr/bin/env node
 
 const bcrypt = require('bcrypt');
-const yargs = require('yargs');
+const { parseArgs } = require('node:util');
 
-const argv = yargs
-    .demandCommand(1)
-    .usage('Usage: $0 [password] - This script helps to generate the bcrypted hash of given password.')
-    .options({
-        verbose: { alias: 'v', type: 'boolean', default: false },
-        rounds: { alias: 'r', type: 'number', default: 3 }
-    })
-    .argv;
+const usage = 'Usage: make-password [password] - This script helps to generate the bcrypted hash of given password.';
 
-const rounds = argv['rounds'];
-const password = argv._[0];
-const verbose = argv['verbose'];
+const { values, positionals } = parseArgs({
+    allowPositionals: true,
+    options: {
+        verbose: { type: 'boolean', short: 'v', default: false },
+        rounds: { type: 'string', short: 'r', default: '3' }
+    }
+});
 
-if (verbose) {
+const password = positionals[0];
+
+if (!password) {
+    console.error(usage);
+    process.exit(1);
+}
+
+const rounds = Number(values.rounds);
+
+if (!Number.isInteger(rounds) || rounds < 1) {
+    console.error(`Invalid rounds value: "${values.rounds}". Expected a positive integer.`);
+    process.exit(1);
+}
+
+if (values.verbose) {
     console.log(`Hashing "${password}" with ${rounds} number of rounds!\n`);
 }
 
 bcrypt.hash(password.toString(), rounds, (err, hash) => {
     if (err) {
         console.error(err);
+        process.exit(1);
     } else {
         console.log(hash);
     }
 });
-
