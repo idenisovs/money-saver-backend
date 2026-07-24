74716b1fa557d465981104beb8ea5f56e7b62d3e
Ilja Denisovs <i.denisovs@gmail.com>
2026-07-24 15:08:29 +0300

    Kill daemon if database path is not set or database is missing.

--- source/dal/connectors/sqlite.ts
+++ source/dal/connectors/sqlite.ts
@@ -1,5 +1,7 @@
+import { existsSync } from 'fs';
+
 import { getLogger } from 'log4js';
-import { Database as SQLite } from 'sqlite3';
+import { Database as SQLite, OPEN_READWRITE } from 'sqlite3';
 
 import enableForeignKeys from './enable-foreign-keys';
 import config from '../../config';
@@ -8,12 +10,23 @@ const log = getLogger('db');
 
 export default function connectSqlite() {
 	if (!config.DATABASE) {
-		throw new Error('DATABASE must be set!');
+		log.fatal('DATABASE must be set!');
+		process.exit(1);
+	}
+
+	if (!existsSync(config.DATABASE)) {
+		log.fatal('Database file is not found at the given path - %s', config.DATABASE);
+		process.exit(1);
 	}
 
 	log.debug('Connecting to %s...', config.DATABASE);
 
-	const db = new SQLite(config.DATABASE);
+	const db = new SQLite(config.DATABASE, OPEN_READWRITE, (err) => {
+		if (err) {
+			log.fatal('Failed to open database "%s": %s', config.DATABASE, err.message);
+			process.exit(1);
+		}
+	});
 
 	enableForeignKeys(db).then(() => {
 		log.debug('%s connected!', config.DATABASE);
