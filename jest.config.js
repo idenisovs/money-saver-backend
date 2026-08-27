b129fe6eed2a0016300b64227ef61a4186ee84a4
Ilja Denisovs <i.denisovs@gmail.com>
2026-08-27 16:29:15 +0300

    Development config updates.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   globalSetup: './jest.setup.js',
   setupFiles: [],
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { noEmitOnError: false } }],
+  },
   testMatch: ['**/*.test.ts'],
   testEnvironment: 'node',
 };
